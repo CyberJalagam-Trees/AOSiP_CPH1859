0f1f28ff376647f1378db4c5ce28f3540fcde909
JAISHNAVPRASAD <62203009+CyberJalagam@users.noreply.github.com>
2020-08-15 14:37:38 +0530

    CPH1859: Inherit AOSiP Stuff

--- aosp_CPH1859.mk
+++ aosp_CPH1859.mk
@@ -18,8 +18,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common AospExtended stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common AOSiP stuff
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Inherit from oppo CPH1861
 $(call inherit-product, device/oppo/CPH1859/device.mk)
